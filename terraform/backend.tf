f2921a1591af90c301f64a7333c695eca6cd5c34
Shiva Velpula <codefob@outlook.com>
2026-06-30 16:17:25 +0000

    Update Terraform  and  ACR config files

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,3 @@
-
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "cloud-shell-storage-southcentralus"
-    storage_account_name = "cs710032000b775b1d7"
-    container_name       = "tfstate"
-    key                  = "aks.tfstate"
-    use_azuread_auth     = true
-  }
+  backend "azurerm" {}
 }
