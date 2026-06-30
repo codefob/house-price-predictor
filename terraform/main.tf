e99272c88a404bef81d7410df2965acb7c552328
Shiva Velpula <codefob@outlook.com>
2026-06-30 20:22:38 +0000

    Deploy Argo CD to AKS using Terraform Helm provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,10 +79,62 @@ locals {
     ? azurerm_kubernetes_cluster.aks[0].kubelet_identity[0].object_id
     : data.azurerm_kubernetes_cluster.existing_aks[0].kubelet_identity[0].object_id
   )
+
+  aks_host = (
+    var.create_aks
+    ? azurerm_kubernetes_cluster.aks[0].kube_config[0].host
+    : data.azurerm_kubernetes_cluster.existing_aks[0].kube_config[0].host
+  )
+
+  aks_client_certificate = (
+    var.create_aks
+    ? azurerm_kubernetes_cluster.aks[0].kube_config[0].client_certificate
+    : data.azurerm_kubernetes_cluster.existing_aks[0].kube_config[0].client_certificate
+  )
+
+  aks_client_key = (
+    var.create_aks
+    ? azurerm_kubernetes_cluster.aks[0].kube_config[0].client_key
+    : data.azurerm_kubernetes_cluster.existing_aks[0].kube_config[0].client_key
+  )
+
+  aks_cluster_ca_certificate = (
+    var.create_aks
+    ? azurerm_kubernetes_cluster.aks[0].kube_config[0].cluster_ca_certificate
+    : data.azurerm_kubernetes_cluster.existing_aks[0].kube_config[0].cluster_ca_certificate
+  )
 }
 
 resource "azurerm_role_assignment" "acr_pull" {
   principal_id         = local.kubelet_object_id
   role_definition_name = "AcrPull"
   scope                = data.azurerm_container_registry.acr.id
-}  
+}
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks,
+    azurerm_role_assignment.acr_pull
+  ]
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  create_namespace = false
+
+  values = [
+    file("${path.module}/argocd-values.yaml")
+  ]
+
+  depends_on = [
+    kubernetes_namespace.argocd
+  ]
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,7 +1,19 @@
 terraform {
+  required_version = ">= 1.6.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.35"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
       version = "~> 3.0"
     }
   }
@@ -9,5 +21,20 @@ terraform {
 
 provider "azurerm" {
   features {}
-  use_oidc = true
+}
+
+provider "kubernetes" {
+  host                   = local.aks_host
+  client_certificate     = base64decode(local.aks_client_certificate)
+  client_key             = base64decode(local.aks_client_key)
+  cluster_ca_certificate = base64decode(local.aks_cluster_ca_certificate)
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = local.aks_host
+    client_certificate     = base64decode(local.aks_client_certificate)
+    client_key             = base64decode(local.aks_client_key)
+    cluster_ca_certificate = base64decode(local.aks_cluster_ca_certificate)
+  }
 }
